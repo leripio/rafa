94f4db4f14ad5b3538c3ec262d5cc28e7be72f30
Henrique Assunção <henrique.mello1992@gmail.com>
2019-06-26 12:23:48 -0300

    Creates a file with functions to list models of forecast

---
+++ R/rafa_list_models.R
@@ -0,0 +1,34 @@
+#' Creates list of forecast models to use
+#'
+#' This function creates a list of models to later use in automatic forecast
+#' @exclude One or more models to exclude from evaluation, eg. "auto.arima" or c("tbats", "nnetar").
+#' @return A list of models.
+#' @export
+#' @examples
+#' rafa_list_functions(exclude = NULL)
+#' rafa_list_functions(exclude = "ses")
+
+rafa_list_models <- function(exclude = NULL){
+  mod_fun <- list("auto.arima"  = function(x, h) forecast::forecast(forecast::auto.arima(x), h= h),
+                  "tbats"  = function(x, h) forecast::forecast(forecast::tbats(x), h= h),
+                  "ets"    = function(x, h) forecast::forecast(forecast::ets(x), h= h),
+                  "nnetar" = function(x, h) forecast::forecast(forecast::nnetar(x), h= h),
+                  "hw"     = function(x, h) forecast::hw(x, h= h),
+                  "arfima" = function(x, h) forecast::forecast(forecast::arfima(x), h= h),
+                  "holt" = function(x, h) forecast::holt(x, h= h),
+                  "thetaf" = function(x, h) forecast::thetaf(x, h= h),
+                  "ses" = function(x, h) forecast::ses(x, h= h),
+                  "meanf" = function(x, h) forecast::meanf(x, h= h),
+                  "splinef" = function(x, h) forecast::splinef(x, h= h),
+                  "StructTS" = function(x,h) forecast::forecast(stats::StructTS(x), h= h),
+                  "elm" = function(x,h) forecast::forecast(nnfor::elm(x), h=h))
+
+  # Allow the exclusion of specific models
+  if(!is.null(exclude)){
+    for(i in seq_along(exclude))
+    {mod_fun[[exclude[[i]]]] <- NULL}
+  }
+  mod_fun
+}
+
+
